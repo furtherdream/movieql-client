d4dade58d5d73a01882b37fb0540ed0868d7fbff
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-28 13:29:27 +0900

    conclusion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React, { Component } from "react"
-import { ApolloProvider } from "react-apollo"
-import { HashRouter as Router, Route } from "react-router-dom"
-import client from "./ApolloClient"
-import Home from "./Home"
-import Detail from "./Detail"
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <Router>
-          <main>
-            <Route exact={true} path={"/"} component={Home} />
-            <Route path={"/details/:movieID"} component={Detail} />
-          </main>
-        </Router>
-      </ApolloProvider>
-    )
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
